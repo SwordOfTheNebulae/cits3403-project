cef50e417df008b5f04c0dea84848422628ad876
SwordOfTheNebulae <49628441+SwordOfTheNebulae@users.noreply.github.com>
2025-05-12 22:07:29 +0800

    fix db populator and populate db
    db_populator.py now ignores duplicate movies, which was what was causing the error before
    database now contains the movies

--- db_populator.py
+++ db_populator.py
@@ -5,12 +5,17 @@ from app.models import Movie
 def populate_movies():
     with open("data/films.csv", mode="r", encoding="utf-8") as file:
         reader = csv.DictReader(file)
+        already_seen = set()
         for row in reader:
+            title = row["title"]
+            year = int(row["release_year"])
+            if((title,year) in already_seen): continue # skip duplicates
+            already_seen.add((title,year))
             movie = Movie(
-                title=row["title"],
+                title=title,
                 genre=row["genre"],
                 avg_rating=float(row["avg_rating"]),
-                release_year=int(row["release_year"])
+                release_year=year
             )
             db.session.add(movie)
         db.session.commit()
