37eb0ce84aa35264ed8cbdb206b452073774d4c7
WeimanGao <24084355@student.uwa.edu.au>
2025-05-16 17:35:35 +0800

    Create test_database_admin.py

---
+++ tests/test_database_admin.py
@@ -0,0 +1,13 @@
+# tests/test_database_admin.py
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from app.create_app import create_app
+from app.models.models import User
+
+def test_admin_account_exists():
+    app = create_app()
+    with app.app_context():
+        admin = User.query.filter_by(username='admin').first()
+        assert admin is not None
