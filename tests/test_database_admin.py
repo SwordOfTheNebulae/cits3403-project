00e4afe1353aa32fe495fcefdc799f709055c2ae
WeimanGao <24084355@student.uwa.edu.au>
2025-05-16 18:38:52 +0800

    Update test_database_admin.py

--- tests/test_database_admin.py
+++ tests/test_database_admin.py
@@ -3,8 +3,8 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from app.create_app import create_app
-from app.models.models import User
+from create_app import create_app
+from models.models import User
 
 def test_admin_account_exists():
     app = create_app()
