966d790b1943f3f6ee2fd46a5770a2fae2f8d1f6
WeimanGao <24084355@student.uwa.edu.au>
2025-05-16 13:59:29 +0800

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,4 +1,8 @@
 # tests/test_routes.py
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from app.create_app import create_app
 
 def test_login_route():
